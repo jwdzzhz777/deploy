9fb6ee597b26fa7d71501b8c47989ba0340719ec
jwdzzhz777 <jwdzzhz777@gmail.com>
2019-12-19 15:43:42 +0800

    feat:幂等性

--- deploy.js
+++ deploy.js
@@ -3,14 +3,24 @@ const url = require('url');
 const { spawn } = require('child_process');
 
 const { hostname, port, appToken, uid, router, shell, callback } = require('./config');
+// 同时只允许一个在部署
+let client_deploy_state = 'free';
+let server_deploy_state = 'free';
 const server = http.createServer((req, res) => {
     const route = url.parse(req.url);
     if (route.pathname === router.client) {
+        if (client_deploy_state === 'pending') {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain', 'charset=utf-8');
+            res.end('当前有部署任务进行中\n');
+        }
         console.log('开始客户端部署');
         let sh = spawn('sh', shell.client);
+        client_deploy_state = 'pending';
         sh.stdout.on('data', data => console.log(`${data}`));
         sh.stderr.on('data', data => console.error(`${data}`));
         sh.on('close', (code) => {
+            client_deploy_state = 'free';
             if (+code === 0) {
                 let params = new URLSearchParams(`appToken=${appToken}&content=部署成功&uid=${uid}`);
                 http.get(`${callback}/?${params.toString()}`, () => {
